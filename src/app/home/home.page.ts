50785ca2354ee0d373c91b8b9c6d0c96e2f1e175
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-12-04 17:43:12 +0100

    fab button update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,17 +9,4 @@ import { DataService, Message } from '../services/data.service';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-  private data = inject(DataService);
-  constructor() {}
-
-  refresh(ev: any) {
-    setTimeout(() => {
-      (ev as RefresherCustomEvent).detail.complete();
-    }, 3000);
-  }
-
-  getMessages(): Message[] {
-    return this.data.getMessages();
-  }
-}
+export class HomePage {}
